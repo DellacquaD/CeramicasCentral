56321a6b60b6fb1d2429aa475d834cddfe223f66
dellacquad <d.dellacquaruiz@gmail.com>
2025-11-11 16:12:50 -0300

    update admin page

--- frontend/src/components/SearchModal.vue.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {
-    $emit: typeof emit;
-}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
-export default _default;
-declare const emit: (event: "search" | "close", ...args: any[]) => void;
--- frontend/src/views/Offers.vue.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {
-    $emit: typeof __VLS_emit;
-}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
-export default _default;
-declare const __VLS_emit: (event: "add-to-cart", ...args: any[]) => void;
